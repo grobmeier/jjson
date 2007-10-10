ba2ddd38b5bc91892a9d1b16166909086d40fb67
Christian Grobmeier <cg@grobmeier.de>
2007-10-10 16:35:59 +0000

    decode string and decode object integrated

--- src/main/java/de/grobmeier/jjson/utils/JSONDecoder.java
+++ src/main/java/de/grobmeier/jjson/utils/JSONDecoder.java
@@ -1,18 +1,3 @@
-/*
- *  Copyright 2007 Christian Grobmeier 
- *  
- *  Licensed under the Apache License, Version 2.0 (the "License"); 
- *  you may not use this file except in compliance with the License. 
- *  You may obtain a copy of the License at 
- *  
- *  http://www.apache.org/licenses/LICENSE-2.0 
- *  
- *  Unless required by applicable law or agreed to in writing, 
- *  software distributed under the License is distributed 
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
- *  either express or implied. See the License for the specific 
- *  language governing permissions and limitations under the License.
- */
 package de.grobmeier.jjson.utils;
 
 import de.grobmeier.jjson.JSONObject;
@@ -20,34 +5,185 @@ import de.grobmeier.jjson.JSONString;
 import de.grobmeier.jjson.JSONValue;
 
 public class JSONDecoder {
+    private JSONReader reader = null;
     /**
      * 
      */
-    public JSONDecoder() {
+    public JSONDecoder(final String json) {
+        reader = new JSONReader(json);
     }
     
-    public JSONValue decode(String input) {
-        if(input.startsWith("\"") && input.endsWith("\"")) {
-            return decodeString(input);
-        } else if(input.startsWith("{") && input.endsWith("}")) {
-            return decodeObject(input);
+   
+    enum Opener {
+        jsonobject('{'), jsonarray('['), jsonstring('"');
+        
+        private char sign = ' ';
+        
+        private Opener(char s) {
+            this.sign = s;
+        }
+    }
+    
+    enum Closer {
+        jsonobject('}'), jsonarray(']'), jsonstring('"');
+        
+        private char sign = ' ';
+        
+        private Closer(char s) {
+            this.sign = s;
+        }
+    }
+
+    /**
+     * 
+     */
+    private class JSONReader {
+        private char[] json = null;
+        private int index = 0;
+        /**
+         * @param _json
+         */
+        public JSONReader(final String _json) {
+            json = _json.trim().toCharArray();
+        }
+        
+        /**
+         * Reads one character and points to the next sign.
+         * @return the read character
+         */
+        public char read() {
+            char result = ' ';
+            if(json.length != 0) {
+                result = json[index];
+            }
+             
+            if(index + 1 < json.length) {
+                index++;
+            } else {
+                index = 0;
+            }
+            return result;
+        }
+        
+        /**
+         * @return
+         */
+        public char current() {
+            return json[index];
+        }
+        
+        /**
+         * @return
+         */
+        public boolean next() {
+            if(index + 1 >= json.length) {
+                index = 0;
+                return false;
+            } else {
+                index++;
+                return true;
+            }
+        }
+        /**
+         * @return
+         */
+        public char readBefore() {
+            if(index - 1 >= 0 && index < json.length) {
+                return json[index-1];
+            } 
+            return ' '; 
+        }
+    }
+
+    /**
+     * @param c
+     * @return
+     */
+    private boolean isOpener(char c) {
+        if(reader.readBefore() == '\\') {
+            return false;
+        }
+        Opener[] opener = Opener.values();
+        for (Opener item : opener) {
+            if(item.sign == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * @param c
+     * @return
+     */
+    private boolean isCloser(char c) {
+        if(reader.readBefore() == '\\') {
+            return false;
+        }
+        Closer[] closer = Closer.values();
+        for (Closer item : closer) {
+            if(item.sign == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public JSONValue decode() {
+        if(reader.current() == Opener.jsonobject.sign) {
+            return decodeObject();
+        } else if(reader.current() == Opener.jsonstring.sign) {
+            return decodeString();
         }
         return null;
     }
     
-    private JSONObject decodeObject(String input) {
-        String temp = input.substring(1, input.length());
-        // nach key suchen
-        // alles nach doppelpunkt bis zum komma in decode schicken
-        // Objekte ablegen, JSONObject instanziieren und zurück
-        return null;
+    private JSONObject decodeObject() {
+        JSONObject result = new JSONObject();
+        
+        boolean hasNext = true;
+        while(hasNext) {
+            // key must be a string
+            reader.next();
+            JSONString key = decodeString();
+            
+            while(reader.next()) {
+                char temp = reader.current();
+                if(temp == ':') {
+                    reader.next();
+                    break;
+                }
+            }
+            
+            JSONValue value = decode();
+            result.put(key.toString(), value);
+            reader.next();
+            if(reader.current() == Closer.jsonobject.sign) {
+                hasNext = false;
+            } 
+        }
+        return result;
     }
     /**
      * @param input
      * @return
      */
-    private JSONString decodeString(String input) {
-        String temp = input.substring(1, input.length());
-        return new JSONString(temp);
+    private JSONString decodeString() {
+        StringBuilder result = new StringBuilder();
+        if(reader.current() == Opener.jsonstring.sign) {
+            while(reader.next()) {
+                char temp = reader.current();
+                // Strings cannot have a opener inside
+                if(isCloser(temp)) {
+                    break;
+                } else {
+                    result.append(temp);
+                }
+            }
+        } else {
+            return new JSONString("");
+        }
+        
+        return new JSONString(result.toString());
     }
 }
--- src/test/java/de/grobmeier/jjson/utils/JSONDecoderTest.java
+++ src/test/java/de/grobmeier/jjson/utils/JSONDecoderTest.java
@@ -1,18 +1,3 @@
-/*
- *  Copyright 2007 Christian Grobmeier 
- *  
- *  Licensed under the Apache License, Version 2.0 (the "License"); 
- *  you may not use this file except in compliance with the License. 
- *  You may obtain a copy of the License at 
- *  
- *  http://www.apache.org/licenses/LICENSE-2.0 
- *  
- *  Unless required by applicable law or agreed to in writing, 
- *  software distributed under the License is distributed 
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
- *  either express or implied. See the License for the specific 
- *  language governing permissions and limitations under the License.
- */
 package de.grobmeier.jjson.utils;
 
 import junit.framework.TestCase;
@@ -24,10 +9,30 @@ import de.grobmeier.jjson.JSONValue;
 public class JSONDecoderTest {
 
     @Test
-    public final void testDecode() {
-        JSONDecoder decoder = new JSONDecoder();
-        JSONValue result = decoder.decode("\"hello, its \"ME\" again!\"");
-        TestCase.assertEquals("\"hello, its \"ME\" again!\"\"",result.toJSON());
+    public final void testDecodeSimpleString() {
+        JSONDecoder decoder = new JSONDecoder("\"hello");
+        JSONValue result = decoder.decode();
+        TestCase.assertEquals("\"hello\"",result.toJSON());
+    }
+    
+    @Test
+    public final void testDecodeString() {
+        JSONDecoder decoder = new JSONDecoder("\"hello, its \\\"ME\\\" again!\"");
+        JSONValue result = decoder.decode();
+        TestCase.assertEquals("\"hello, its \\\"ME\\\" again!\"",result.toJSON());
+    }
+    
+    @Test
+    public final void testDecodeObject() {
+        JSONDecoder decoder = new JSONDecoder("{\"key\":\"value\"}");
+        JSONValue result = decoder.decode();
+        System.out.println(result.toJSON());
     }
 
+    @Test
+    public final void testDecodeObject2() {
+        JSONDecoder decoder = new JSONDecoder("{\"key\":\"value\",\"key2\":\"value2\"}");
+        JSONValue result = decoder.decode();
+        System.out.println(result.toJSON());
+    }
 }
